b59c3c317aeeecf6b94e0edd5906972b80b6cc53
Dharani Inkollu <inkolludharani2508@gmail.com>
2023-02-28 16:54:16 +0530

    Stacks and Queues Theory

---
+++ stacksAndQueues/InBuiltExamples.java
@@ -0,0 +1,44 @@
+package stacksAndQueues;
+
+import java.util.*;
+
+//using in-built java stack and queue
+public class InBuiltExamples {
+    public static void main(String[] args) {
+    	
+    	System.out.println();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(45);
+        stack.push(2);
+        stack.push(9);
+        stack.push(18);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        //System.out.println(stack.pop());  --throws exception
+
+        
+        
+        //******************imp*************
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(3);
+        queue.add(6);
+        queue.add(5);
+        queue.add(19);
+        queue.add(1);
+
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(89);
+        deque.addLast(78);
+
+        System.out.println(deque.removeFirst());
+        // etc etc
+    }
+}
---
+++ stacksAndQueues/StackException.java
@@ -0,0 +1,9 @@
+package stacksAndQueues;
+
+@SuppressWarnings("serial")
+public class StackException extends Exception{
+
+    public StackException(String message) {
+        super(message);
+    }
+}
